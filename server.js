51ddecef692e633b6fd1ce4302d25e2158663dd2
atomnoid <aayushs2580@gmail.com>
2025-05-04 14:44:08 +0530

    nthng

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
